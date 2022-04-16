d608de56cbd2cfb2bd164862ba049620067c1b2e
gouravgopal20 <101074973+gouravgopal20@users.noreply.github.com>
2022-04-16 10:26:14 +0530

    Add files via upload

---
+++ para.h
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   para.h
+ * Author: gourav
+ *
+ * Created on 15 April, 2022, 11:37 PM
+ */
+
+#ifndef PARA_H
+#define  PARA_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#define FROM "<dummybeartest@gmail.com>" // pass it to smtp from
+#define TO "<1905015cse@cit.edu.in>" // pass it to smtp from
+#define MAIL_HEADER_FROM "gourav<dummybeartest@gmail.com>" // pass it to header  
+#define MAIL_HEADER_TO "Mini<1905015cse@cit.edu.in>"   // pass it to header
+#define UID "(your mail username)"
+#define PWD "(your mail password)"
+#define smtp_server_url "smtp.gmail.com"
+#define domain_name "localhost"
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif  /* PARA_H */
+
